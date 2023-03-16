4ae70d3cf9c37f3164f1b8535137fc25c8d8fad4
Replit user <>
2023-03-16 16:11:55 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -91,7 +91,8 @@ la cual asigna el valor a la posición i del vector v.
 */
 void asignarValor(Vector * v, int i, int valor) {
   for ( int k = 0; k > i ; k++){
-    v[k] = valor;
+    scanf("%i", &valor);
+    v[k] = valor
     
   }
 }
