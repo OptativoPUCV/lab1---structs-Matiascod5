bfe2e1acf8d8b70a62a715c60c76569e408266a0
Replit user <>
2023-03-16 16:05:35 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -29,7 +29,7 @@ void arrayMaxMin(int *a, int n, int *max, int *min) {
 int mayor = 0;
 int menor = 0;
 
-  for ( int i = 0 ; i > n ; i++){
+  for ( int i = 0 ; i < n ; i++){
     if ( a[i] > a[i + 1] ){
       mayor = a[i];
     }
